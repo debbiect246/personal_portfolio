2c1c6e395cf3db8fbf4c4f690a995fc3bf82da58
Debbie Thompson <deb246@btinternet.com>
2020-05-06 21:24:37 +0100

    getting blogs from back end to show on front end

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-# Create your models here.
+# Blog model created below
+
+
+class Blog(models.Model):
+    title = models.CharField(max_length=200)
+    description = models.TextField(max_length=250)
+    date = models.DateField()
--- blog/urls.py
+++ blog/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from . import views
+from .import views
 
 urlpatterns = [
     path('', views.all_blogs, name='all_blogs'),
